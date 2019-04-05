5ba4a21bdc531efe3d170487d4acb6968b516e3b
dyerw <liamd94@gmail.com>
2019-04-04 23:22:01 -0500

    You can move yr dudes around

--- Game.gd
+++ Game.gd
@@ -1,15 +1,32 @@
 extends Node2D
 const MapGenUtil = preload("utils/MapGen.gd")
 
-
 var map
+var selected_entity
+var entities = []
+
 onready var DungeonMap = get_node("DungeonMap")
 onready var Entity = preload("res://scenes/Entity.tscn")
+
+func is_entity_at_position(pos: Vector2) -> bool:
+	for entity in entities:
+		if entity.gridX == pos.x and entity.gridY == pos.y:
+			return true
+	return false
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton \
+    and event.button_index == BUTTON_LEFT \
+    and event.is_pressed():
+		var map_position = DungeonMap.world_to_map(event.position)
+		if selected_entity and not is_entity_at_position(map_position):
+			selected_entity.move(map_position.x, map_position.y)
 
 func _add_player_unit(x, y):
 	var e = Entity.instance()
 	e.init(x, y)
 	add_child(e)
+	return e
 
 func _ready():
 	OS.set_window_size(Vector2(16 * 30, 16 * 30))
@@ -17,6 +34,9 @@ func _ready():
 	map = MapGenUtil.gen_map(30, 30, 0)
 	DungeonMap.draw_map(map)
 	
-	_add_player_unit(0, 0)
-	_add_player_unit(1, 0)
-	_add_player_unit(29, 0)
+	entities.push_back(_add_player_unit(0, 0))
+	entities.push_back(_add_player_unit(1, 0))
+	entities.push_back(_add_player_unit(29, 0))
+
+func child_clicked(node):
+	selected_entity = node
--- scenes/Entity.gd
+++ scenes/Entity.gd
@@ -2,19 +2,20 @@ extends Node2D
 
 var gridX
 var gridY
+var id
 
 func init(x, y):
 	gridX = x
 	gridY = y
 
-func move(x, y):
+func move(x: int, y: int):
 	gridX = x
 	gridY = y
 	self.position.x = gridX * 16
 	self.position.y = gridY * 16
 
 func on_click():
-	print("clicked!")
+	get_parent().child_clicked(self)
 
 func _ready():
 	move(gridX, gridY)
