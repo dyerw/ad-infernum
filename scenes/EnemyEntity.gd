0394794812b5771ddf7a14af7936ca31a2b15669
dyerw <liamd94@gmail.com>
2019-04-05 20:56:46 -0500

    Remove debug script

--- scenes/EnemyEntity.gd
+++ scenes/EnemyEntity.gd
@@ -9,7 +9,6 @@ func on_click():
 func end_turn():
 	var target_path = get_parent().get_path_to_nearest_player_unit(Vector2(self.gridX, self.gridY))
 	target_path.remove(target_path.size() - 1)
-	print(target_path)
 	if target_path.size() > 0:
 		.move_along_path(target_path)
 	.end_turn()
